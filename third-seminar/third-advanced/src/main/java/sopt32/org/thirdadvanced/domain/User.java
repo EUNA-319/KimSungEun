887334cc8d6b5609435c5e153234844c55827014
fascinatioeuna <81363864+EUNA-319@users.noreply.github.com>
2023-05-05 03:31:06 +0900

    setting basic-seminar content in advanced-seminar

---
+++ third-seminar/third-advanced/src/main/java/sopt32/org/thirdadvanced/domain/User.java
@@ -0,0 +1,31 @@
+package sopt32.org.thirdadvanced.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder // 빌더 에노테이션
+    public User(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+}
---
+++ third-seminar/third-advanced/src/main/java/sopt32/org/thirdadvanced/infrastructure/UserRepository.java
@@ -0,0 +1,8 @@
+package sopt32.org.thirdadvanced.infrastructure;
+
+import org.springframework.data.repository.Repository;
+import sopt32.org.thirdadvanced.domain.User;
+
+public interface UserRepository extends Repository<User, Long> {
+    void save(User user); // 유저 회원 가입
+}
